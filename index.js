d9b1a8a26b638cfdbd78ae6d5297b7089f2ae7c6
Xu Zhiwei <gcdxuzhiwei@gmail.com>
2024-04-08 11:23:30 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
+const axios = require("axios");
 
+axios
+  .post("https://fl9web01.fastlinkyj.com/auth/login", {
+    email: process.env.EMAIL,
+    passwd: process.env.PASSWD,
+    code: "",
+    remember_me: "on",
+  })
+  .then((res) => {
+    const cookie = res.headers["set-cookie"]
+      .map((v) => v.split("; ")[0])
+      .join("; ");
 
-console.log(process.env)
+    axios
+      .post("https://fl9web01.fastlinkyj.com/user/checkin", undefined, {
+        headers: {
+          cookie,
+        },
+      })
+      .then((res) => {
+        console.log("---res---", res.data);
+      });
+  });
